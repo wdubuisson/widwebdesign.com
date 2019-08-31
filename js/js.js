17007ba023e69661b7047069b155e4101699782d
Widmarc <wdubu001@gmail.com>
2019-08-30 22:18:37 -0400

    initial commit

---
+++ js/js.js
@@ -0,0 +1,13 @@
+function changeBackground() {
+
+    var text = document.getElementById("project1").style.backgroundColor = "red";
+
+
+}
+
+function backToNormal() {
+
+    var text = document.getElementById("project1").style.backgroundColor = "";
+
+
+}
---
+++ js/main.js
@@ -0,0 +1,13 @@
+function changeBackground() {
+
+    var text = document.getElementById("project1").style.Color = "black";
+
+
+}
+
+function backToNormal() {
+
+    var text = document.getElementById("project1").style.Color = "";
+
+
+}
